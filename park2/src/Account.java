e0b944c0b2ce529e28db9cbd2054922486a244bb
KAU <KAU@KAU-PC>
2017-03-20 16:45:26 +0900

    first commit

---
+++ park2/src/Account.java
@@ -0,0 +1,22 @@
+
+public class Account {
+	
+	private double balance;
+	
+	public double getbalance(){
+		return balance;
+	}
+	
+	public void setbalance(double a){
+	balance = a;
+	}
+	
+	public void debit(double a, double b){
+		
+		balance
+		
+	}
+	
+	
+
+}
